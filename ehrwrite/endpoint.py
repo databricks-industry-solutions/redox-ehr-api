3cc3b06f4b9e4ecdff39c0bd89128c3c91c7ef15
Emma Yamada <emma.yamada@databricks.com>
2024-10-23 08:56:47 -0400

    add api request class

--- ehrwrite/endpoint.py
+++ ehrwrite/endpoint.py
@@ -1,25 +1,35 @@
 """
  Class to handle writing to endpoints
-
-   kwargs = specific endpoint 
 """
 
+import requests
+import datetime
+import json
 
-class RedoxApi():
-
-    def __init__(self, auth):
+class RedoxApiRequest:
+    def __init__(self, auth, base_url):
         self.auth = auth
+        self.base_url = base_url
 
+    def make_request(self, http_method, resource, interaction, data=None):
+        method = getattr(requests, http_method)
+        endpoint = f"{resource}/{interaction}"
+        url = f"{self.base_url}{endpoint}"
 
-    def write_record(self, endpoint, action, data, retries = 3, **kwargs):
-        pass #TODO
+        auth_result = self.auth.get_token()
 
-    @pandas_udf
-    def write_partition(self, endpoint, action, data, **kwargs): 
-        pass #TODO
+        headers = {
+            'Authorization': f"Bearer {auth_result['access_token']}",
+            'Content-Type': 'application/json'
+        }
 
+        response = method(url, headers=headers, data=data)
 
-    def write_df(self, df, num_connections, **kwargs):
-        pass #TODO 
-
-    
+        if response.status_code == 200:
+            try:
+                print(f"{datetime.datetime.now()} - {response.json()}")
+            except json.JSONDecodeError:
+                print(f"{datetime.datetime.now()} - Failed to decode JSON from response.")
+        else:
+            print(f"{datetime.datetime.now()} - Request failed with status code: {response.status_code}")
+            print(f"{datetime.datetime.now()} - Error message from endpoint: {response.text}")
