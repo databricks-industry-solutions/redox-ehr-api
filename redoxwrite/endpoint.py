b17367f31d8a98de07e4a50f0b8ab5ff99090c65
Aaron Z <aaron.zavora@databricks.com>
2024-12-30 13:54:07 -0500

    updates

--- redoxwrite/endpoint.py
+++ redoxwrite/endpoint.py
@@ -2,18 +2,20 @@
  Class to handle writing to endpoints
 """
 
+import requests
+
 class RedoxApiRequest:
     def __init__(self, auth, base_url):
         self.auth = auth
         self.base_url = base_url
 
     def make_request(self, http_method, resource, action, data=None):
-        response = getattr(requests, http_method)(f"{self.base_url}/{resource}/{action}", headers=headers, data=data, auth=auth)
+        response = getattr(requests, http_method)(f"{self.base_url}{resource}/{action}", data=data, auth=self.auth)
         
         return {'request':
                 {
                     'http_method': http_method,
-                    'url': f"{self.base_url}/{resource}/{action}",
+                    'url': f"{self.base_url}{resource}/{action}",
                 },
                 'response': {
                     'response_status_code': response.status_code, 
